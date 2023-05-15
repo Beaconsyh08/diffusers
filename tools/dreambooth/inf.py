907a84b2db1e7171c60480e5fb7001fa1c86d530
Yuhao Song <songyuhao@haomo.ai>
2023-05-15 22:57:53 +0800

    modi shellscript and add utils

--- tools/dreambooth/inf.py
+++ tools/dreambooth/inf.py
@@ -20,24 +20,58 @@
 from diffusers import DiffusionPipeline
 import torch
 import os
+import cv2
+import numpy as np  
 
-prompts = ["a photo of sks night traffic scene", ]
-# prompts = ["a dog in a sks night", ]
+# prompts = ["a street filled with lots of traffic at night time with lights on and cars driving down the street and a building in the background", 
+#            "a street that covered by heavy snow, filled with lots of traffic and cars driving down the street and a building in the background, 4k",
+#            "a street filled with lots of traffic and cars driving down the street and a building in the background, 4k",
+#            "a street filled with lots of traffic and cars driving down the street and a building in the background, rainy, 4k",
+#            "a photo of a dog"]
 
-# prompts = ["a photo of a dog", "a photo of a dog", ]
-model_names = ["haomo_night_sks_200x",]
-model_dir = "./res/finetune/dreambooth" 
-n = 50
+# prompts = ["a street filled with lots of traffic at night time with lights on and cars driving down the street and a building in the background, in the style of haomo", 
+#            "a street that covered by heavy snow, filled with lots of traffic and cars driving down the street and a building in the background, in the style of haomo, 4k",
+#            "a street filled with lots of traffic and cars driving down the street and a building in the background, in the style of haomo, 4k",
+#            "a street filled with lots of traffic and cars driving down the street and a building in the background, rainy, in the style of haomo, 4k",
+#            "a photo of a dog, in the style of haomo"]
+
+# prompts = ["a street filled with lots of traffic and cars driving down the street and a building in the background, rainy, 4k",]
+
+prompts = ["a photo of a dog, in the style of haomo",
+           "a street filled with lots of traffic and cars driving down the street and a building in the background, in the style of haomo, 4k",
+           "a street filled with lots of traffic and cars driving down the street and a building in the background, rainy, in the style of haomo, 4k",]
+
+model_names = ["haomo_5000_hmp"]
+# model_names = [ "haomo_5000"]
+
+# model_dir = "./res/finetune/dreambooth" 
+model_dir = "/mnt/ve_share/generation/models/online/diffusions/res/finetune/dreambooth"
+n = 48
+combine = True
 
 for ind, model_name in enumerate(model_names):
     res_dir = "./vis/dreambooth/%s" % model_name
     os.makedirs(res_dir, exist_ok=True)
     model_id= "%s/%s" % (model_dir, model_name)
     pipe = DiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16).to("cuda")
-    prompt = prompts[ind]
     
-    for i in range(n):
-        image = pipe(prompt, num_inference_steps=50, guidance_scale=7.5).images[0]
-        res_id = "%s/%s_%d.png" % (res_dir, "_".join(prompt.split(" ")), i)
-        image.save(res_id)
-        print(res_id)
+    for prompt in prompts:
+        img_lst = []
+        res_dir_p = "%s/%s" % (res_dir, "_".join(prompt.split(" ")))
+        os.makedirs(res_dir_p, exist_ok=True)
+        
+        for i in range(n):
+            image = pipe(prompt, num_inference_steps=50, guidance_scale=7.5).images[0]
+            res_id = "%s/%d.png" % (res_dir_p, i)
+            image.save(res_id)
+                
+            if combine:
+                image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR) 
+                img_lst.append(image)
+
+        if combine:
+            list_of_lists = [img_lst[i : i + 4] for i in range(0, len(img_lst), 4)]
+            im_combined = cv2.vconcat([cv2.hconcat(_) for _ in list_of_lists])
+            res_id = "%s/combined.png" % (res_dir_p)
+            cv2.imwrite(res_id, im_combined)
+            print(res_id)
--- tools/dreambooth/train_one_online.sh
+++ tools/dreambooth/train_one_online.sh
@@ -1,13 +1,45 @@
 pip install -e ".[torch]"
 
+helpFunction()
+{
+   echo ""
+   echo "Usage: $0 -a INSTANCE_DIR -b INSTANCE_PROMPT -c OUTPUT_DIR"
+   echo -e "\t-a Description of what is INSTANCE_DIR"
+   echo -e "\t-b Description of what is INSTANCE_PROMPT"
+   echo -e "\t-c Description of what is OUTPUT_DIR"
+   exit 1 # Exit script after printing help
+}
+
+while getopts "i:p:o:" opt
+do
+   case "$opt" in
+      i ) INSTANCE_DIR="$OPTARG" ;;
+      p ) INSTANCE_PROMPT="$OPTARG" ;;
+      o ) OUTPUT_DIR="$OPTARG" ;;
+      ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
+   esac
+done
+
+# Print helpFunction in case parameters are empty
+if [ -z "$INSTANCE_DIR" ] || [ -z "$INSTANCE_PROMPT" ] || [ -z "$OUTPUT_DIR" ]
+then
+   echo "Some or all of the parameters are empty";
+   helpFunction
+fi
+
+# Begin script in case all parameters are correct
+echo "$INSTANCE_DIR"
+echo "$INSTANCE_PROMPT"
+echo "$OUTPUT_DIR"
+
 accelerate config default
 export DATA_NAME="haomo"
 export MODEL_NAME="/mnt/ve_share/generation/models/online/diffusions/base/stable-diffusion-v1-5"
 # export INSTANCE_DIR="./data/train/finetune/$DATA_NAME"
-export INSTANCE_DIR="/mnt/ve_share/generation/data/train/diffusions/5000/imgs"
-export OUTPUT_DIR="/mnt/ve_share/generation/models/online/diffusions/res/finetune/dreambooth/${DATA_NAME}_5000_test"
-# export CLASS_DIR="./data/train/finetune/night_class"
-export INSTANCE_PROMPT="/mnt/ve_share/generation/data/train/diffusions/5000/pmps"
+# export INSTANCE_DIR="/share/generation/data/train/diffusions/5000/imgs"
+# export OUTPUT_DIR="/share/generation/models/online/diffusions/res/finetune/dreambooth/${DATA_NAME}_5000_seg1"
+# # export CLASS_DIR="./data/train/finetune/night_class"
+# export INSTANCE_PROMPT="/share/generation/data/train/diffusions/5000/pmps_seg_test1"
 
 accelerate launch --multi_gpu ./examples/dreambooth/train_dreambooth_one.py \
   --pretrained_model_name_or_path=$MODEL_NAME  \
@@ -22,12 +54,14 @@ accelerate launch --multi_gpu ./examples/dreambooth/train_dreambooth_one.py \
   --learning_rate=2e-6 \
   --lr_scheduler="constant" \
   --lr_warmup_steps=0 \
-  --num_class_images=200 \
-  --max_train_steps=80000 \
-  --checkpointing_steps=10000
+  --max_train_steps=6250 \
+  --checkpointing_steps=3125
 
-# 200 for 32000  max_train_steps * train_batch_size
+# 1 for 160  max_train_steps * train_batch_size * gpu
   # --with_prior_preservation --prior_loss_weight=1.0 \
 
   # --class_prompt="a photo of night traffic scene" \
 #   --use_8bit_adam \
+
+
+#  ./tools/dreambooth/train_one_online.sh -i /mnt/ve_share/generation/data/train/diffusions/5000/imgs -p /mnt/ve_share/generation/data/train/diffusions/5000/pmps_seg_test1 -o /mnt/ve_share/generation/models/online/diffusions/res/finetune/dreambooth/haomo_5000_seg1_ttt
