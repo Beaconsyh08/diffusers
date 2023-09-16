467267818c40cb511fc128419dc764357908d46c
Yuhao Song <songyuhao2008@gmail.com>
2023-09-16 17:31:05 +0800

    inf xl ins

--- tools/instructpix2pix/xl/inf.py
+++ tools/instructpix2pix/xl/inf.py
@@ -3,20 +3,18 @@ from diffusers import StableDiffusionXLInstructPix2PixPipeline
 from diffusers.utils import load_image
 
 resolution = 768
-image = load_image(
-    "https://hf.co/datasets/diffusers/diffusers-images-docs/resolve/main/mountain.png"
-).resize((resolution, resolution))
+image = load_image("./mountain.png").resize((resolution, resolution))
 edit_instruction = "Turn sky into a cloudy one"
 
 pipe = StableDiffusionXLInstructPix2PixPipeline.from_pretrained(
-    "/mnt/share_disk/LIV/generation_group/models/diffusers/public/sdxl-instructpix2pix-7688", torch_dtype=torch.float16
+    "/mnt/share_disk/LIV/generation_group/models/diffusers/public/sdxl-instructpix2pix-768", torch_dtype=torch.float16
 ).to("cuda")
 
 edited_image = pipe(
     prompt=edit_instruction,
     image=image,
-    height=resolution,
-    width=resolution,
+    # height=resolution,
+    # width=resolution,
     guidance_scale=3.0,
     image_guidance_scale=1.5,
     num_inference_steps=30,
